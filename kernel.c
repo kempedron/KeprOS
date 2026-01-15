436aa4a38af1b140f0631d5efb0e96f468369ccf
kempedron@gmail.com <kepr@archlinux.localdomain>
2026-01-15 22:49:22 +0300

    for save

--- kernel.c
+++ kernel.c
@@ -13,15 +13,126 @@
 #define VGA_COLOR_BLACK 0x0
 #define VGA_COLOR_BLUE 0x1
 #define VGA_COLOR_GREEN 0x2
-#define NULL ((void *)0)
-// #define COLOR_BLACK 0
-// #define COLOR_LIGHT_GREY 7
-// #define COLOR_WHITE 15
+#define MAX_FILES 16
+#define MAX_FILENAME 32
+#define BLOCK_SIZE 512
+
+// VGA DRIVER INIT
 char *vidmem = (char *)VGA_ADDRESS;
 uint8_t terminal_color = 0x07;
 
 int cursor_x = 0;
 int cursor_y = 0;
+
+// basic functions
+
+char *strcpy(char *dest, const char *src) {
+  char *saved = dest;
+  while ((*dest++ = *src++) && *dest != '\0') {
+  }
+  return saved;
+}
+
+size_t strlen(const char *str) {
+  const char *s = str;
+  while (*s) {
+    s++;
+  }
+  return (size_t)(s - str);
+}
+
+int strcmp(const char *s1, const char *s2) {
+  while (*s1 && (*s1 == *s2)) {
+    s1++;
+    s2++;
+  }
+  return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+}
+
+void *memcpy(void *dest, void *src, size_t n) {
+  unsigned char *d = (unsigned char *)dest;
+  const unsigned char *s = (const unsigned char *)src;
+
+  while (n--) {
+    *d++ = *s++;
+  }
+  return dest;
+}
+
+// File system(RAM)
+struct File {
+  char name[MAX_FILENAME];
+  int size;
+  char data[MAX_FILES];
+  int is_used;
+};
+
+struct File filesystem[MAX_FILES];
+
+void fs_init() {
+  for (int i = 0; i < MAX_FILES; i++) {
+    filesystem->is_used = 0;
+    filesystem->size = 0;
+  }
+}
+
+int fs_find_file(char *name) {
+  for (int i = 0; i < MAX_FILES; i++) {
+    if (filesystem[i].is_used && (strcmp(filesystem[i].name, name))) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+int fs_create_file(char *name) {
+  if (fs_find_file(name) != -1) {
+    return -2;
+  }
+  for (int i = 0; i < MAX_FILES; i++) {
+    if (!filesystem[i].is_used) {
+      strcpy(filesystem[i].name, name);
+      filesystem[i].size = 0;
+      filesystem[i].is_used = 1;
+      return i;
+    }
+  }
+  return -1;
+}
+
+int fs_write_file(char *name, char *content) {
+  int index = fs_find_file(name);
+  if (index == -1)
+    index = fs_create_file(name);
+  if (index >= 0) {
+    int len = strlen(content);
+    if (len > BLOCK_SIZE)
+      len = BLOCK_SIZE;
+    memcpy(filesystem[index].data, content, len);
+    filesystem[index].size = len;
+    return 0;
+  }
+  return -1;
+}
+
+char *fs_read_file(char *name) {
+  int index = fs_find_file(name);
+  if (index != -1) {
+    return filesystem[index].data;
+  }
+  return 0;
+}
+
+void fs_list_files(char arr[MAX_FILES][MAX_FILENAME]) {
+  int file_index = 0;
+
+  for (int i = 0; i < MAX_FILES; i++) {
+    if (filesystem[i].is_used) {
+      memcpy(arr[file_index], filesystem[i].name, MAX_FILENAME);
+      file_index++;
+    }
+  }
+}
 
 // Assembler functions
 
@@ -256,23 +367,81 @@ int tokinaze(char *str, char **argv) {
   }
   return argc;
 }
-int strcmp(const char *s1, const char *s2) {
-  while (*s1 && (*s1 == *s2)) {
-    s1++;
-    s2++;
-  }
-  return *(const unsigned char *)s1 - *(const unsigned char *)s2;
-}
 
-// clear screen cmd func
+// cmd functions templates
 void cmd_clear(int argc, char **argv) { clean_screen(); }
 void cmd_help(int argc, char **argv);
 void cmd_echo(int argc, char **argv);
+void cmd_cat(int argc, char **argv);
+void cmd_write(int argc, char **argv);
+void cmd_touch(int argc, char **argv);
+void cmd_ls(int argc, char **argv);
 
 command_t cmd_table[] = {{"help", "show all commands", cmd_help},
                          {"clear", "clear screen", cmd_clear},
                          {"echo", "printing text to screen", cmd_echo},
+                         {"cat", "read the file", cmd_cat},
+                         {"write", "write data in the file", cmd_write},
+                         {"touch", "creating new file", cmd_touch},
+                         {"ls", "list all files", cmd_ls},
                          {NULL, NULL, NULL}};
+
+// cmd functions full
+
+void cmd_echo(int argc, char **argv) {
+  for (int i = 1; i < argc; i++) {
+    print_string(argv[i]);
+    print_string(" ");
+    print_char('\n');
+  }
+}
+
+void cmd_help(int argc, char **argv) {
+  char *help_str = "Available commands: echo,help,clear\n";
+  print_string(help_str);
+}
+
+void cmd_write(int argc, char **argv) {
+  int result_code = fs_write_file(argv[1], argv[2]);
+  if (result_code == -1) {
+    print_string("\nerror write data in file\n");
+    print_string(argv[1]);
+    print_char('\n');
+  } else {
+    print_string("\n data was successfully writed in file ");
+    print_string(argv[1]);
+    print_char('\n');
+  }
+}
+
+void cmd_cat(int argc, char **argv) {
+  char *date = fs_read_file(argv[1]);
+  print_char('\n');
+  print_string(date);
+  print_char('\n');
+}
+
+void cmd_touch(int argc, char **argv) {
+  int result_code = fs_create_file(argv[1]);
+  if (result_code == -2) {
+    print_string("\nerror: file aldery exist\n");
+  } else {
+    print_string("\nfile ");
+    print_string(argv[1]);
+    print_string(" successfully created!\n");
+  }
+}
+
+void cmd_ls(int argc, char **argv) {
+  char names[MAX_FILES][MAX_FILENAME];
+  fs_list_files(names);
+
+  for (int i = 0; i < MAX_FILES; i++) {
+    if (names[i][0] != '\0') {
+      print_string(names[i]);
+    }
+  }
+}
 
 // parser
 void shell_execute(char *input) {
@@ -290,19 +459,6 @@ void shell_execute(char *input) {
   print_string("error:unknow command: ");
   print_string(argv[0]);
   print_string("\nUse 'help' for view command list\n");
-}
-
-void cmd_echo(int argc, char **argv) {
-  for (int i = 1; i < argc; i++) {
-    print_string(argv[i]);
-    print_string(" ");
-    print_char('\n');
-  }
-}
-
-void cmd_help(int argc, char **argv) {
-  char *help_str = "Available commands: echo,help,clear\n";
-  print_string(help_str);
 }
 
 char get_char() {
@@ -352,11 +508,12 @@ char *read_line(char *buffer, int max_len) {
 
 void os_main(void) {
   char *buffer;
+  fs_init();
   clean_screen();
   print_string("KeprOS is running!\n");
 
   while (1) {
-    print_string("root@keprOS> ");
+    print_string("\nroot@keprOS> ");
     char *line = read_line(buffer, 1000000);
     shell_execute(line);
   }
--- types.h
+++ types.h
@@ -9,6 +9,7 @@ typedef signed short int16_t;
 typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
+#define NULL ((void *)0)
 
 #ifdef __LP64__
 typedef signed long int64_t;
